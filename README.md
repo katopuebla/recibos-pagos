904b652fb1f62a305c541dd9e9dfff35ed76e272
katopuebla <katopuebla@gmail.com>
2020-09-07 18:41:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# recibos-pagos
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/recibos-pagos)
---
+++ pages/ticket/ticket.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.html',
+  styleUrls: ['./ticket.css']
+})
+export class TicketPage{
+
+  constructor(public navCtrl: NavController) { }
+
+}
