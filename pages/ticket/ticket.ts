283498101d0821fe76dcb7fc190f4ed5cdb5bfe0
katopuebla <katopuebla@gmail.com>
2020-12-01 02:56:32 -0600

    show user values

--- pages/ticket/ticket.ts
+++ pages/ticket/ticket.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { UserService } from "../../app/service/user.service";
+import { User } from "../../interface/module";
 
 @Component({
   selector: "app-ticket",
@@ -8,11 +9,13 @@ import { UserService } from "../../app/service/user.service";
   styleUrls: ["./ticket.css"]
 })
 export class TicketPage {
+  users: User = [];
   constructor(public navCtrl: NavController, private service: UserService) {}
 
   ionViewDidLoad() {
-    this.service.getUsers().subscribe((data: any) => {
-      console.log(data);
+    this.service.getUsers().subscribe((data: User[]) => {
+      this.users = data;
+      console.log(this.users);
     });
   }
 }
