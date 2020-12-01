df38e8f6efaf77bcd85cfae99671026e3f5925d3
katopuebla <katopuebla@gmail.com>
2020-12-01 01:04:40 -0600

    UserService change

--- app/service/base.service.ts
+++ app/service/base.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 export class BaseService {
-  getUrlComponent(table: string) {
-    return "holas";
+  BASE_URL =
+    "https://script.google.com/macros/s/AKfycbycpIYrWv113Jd3yNGlGZ8RPEJLQSDsTVfdZ8cT9AdWR9fwm44/exec";
+  SPREAD_SHEET_ID = "1dkH4nBPPto68a474N1cdIXtIjtpOAp3q3yQVUJc8JOQ";
+
+  constructor(private _http: HttpClient) {}
+
+  public getUrlComponent(table: string) {
+    const url =
+      this.BASE_URL + `?spreadsheetId=${this.SPREAD_SHEET_ID}&sheet=${table}`;
+    return this._http.get(url, { responseType: "json" }).toPromise();
   }
 }
