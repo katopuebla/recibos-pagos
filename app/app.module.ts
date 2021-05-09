b070fb73800da23112536e2d0ec362d638059c6a
katopuebla <katopuebla@gmail.com>
2021-05-09 01:05:21 -0500

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-//import { StatusBar } from '@ionic-native/status-bar';
+import { StatusBar } from '@ionic-native/status-bar';
 //import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MesComponent } from './components/mes/mes.component';
@@ -71,7 +71,7 @@ import { RecibosPage } from '../pages/recibos/recibos.page';
     AboutComponent
   ],
   providers: [
-    //StatusBar,
+    StatusBar,
     //SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     BaseService,
