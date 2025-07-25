e06c69f21592b35b065f62f5641e51c02592fbc3
katopuebla <katopuebla@gmail.com>
2025-07-25 05:23:27 +0000

    Deploying to gh-pages from @ katopuebla/recibos-pagos@5386e956f710fde53b20e3f6be1aedcc69372bf5 🚀

--- dev/198.a4e7043ade21e8cd.js
+++
@@ -1,8 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[198],{198:(p,c,o)=>{o.r(c),o.d(c,{ion_text:()=>a});var r=o(1734),_=o(1882),u=o(9710);
-/*!
-       * (C) Ionic http://ionicframework.com - MIT License
-       */const a=class{constructor(s){(0,r.r)(this,s),this.color=void 0}render(){const s=(0,u.b)(this);return(0,r.h)(r.f,{key:"e134d70c04344b708a2ecf6253722781ad2ca826",class:(0,_.c)(this.color,{[s]:!0})},(0,r.h)("slot",{key:"da79c760f7ebbcd007ce110439f05a62cb22eac8"}))}};a.style=":host(.ion-color){color:var(--ion-color-base)}"},1882:(p,c,o)=>{o.d(c,{c:()=>u,g:()=>l,h:()=>_,o:()=>s});var r=o(9204);
-/*!
-       * (C) Ionic http://ionicframework.com - MIT License
-       */const _=(t,e)=>null!==e.closest(t),u=(t,e)=>"string"==typeof t&&t.length>0?Object.assign({"ion-color":!0,[`ion-color-${t}`]:!0},e):e,l=t=>{const e={};return(t=>void 0!==t?(Array.isArray(t)?t:t.split(" ")).filter(n=>null!=n).map(n=>n.trim()).filter(n=>""!==n):[])(t).forEach(n=>e[n]=!0),e},a=/^[a-z][a-z0-9+\-.]*:/,s=function(){var t=(0,r.A)(function*(e,n,d,h){if(null!=e&&"#"!==e[0]&&!a.test(e)){const i=document.querySelector("ion-router");if(i)return null!=n&&n.preventDefault(),i.push(e,d,h)}return!1});return function(n,d,h,i){return t.apply(this,arguments)}}()}}]);
-//# sourceMappingURL=198.a4e7043ade21e8cd.js.map
