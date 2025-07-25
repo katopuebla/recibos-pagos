ee83ca458ca94b5241788ecd476f18786cb05d4c
katopuebla <katopuebla@gmail.com>
2025-07-25 05:20:00 +0000

    Deploying to gh-pages from @ katopuebla/recibos-pagos@fb32fcc2b2502d31b81e0ef8c6f04cf5b5cf8d7b 🚀

---
+++ dev/1791.03b3f8ecc1cd24cf.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[1791],{1791:(h,s,e)=>{e.r(s),e.d(s,{startStatusTap:()=>i});var a=e(9204),o=e(1734),_=e(8710),d=e(1895);
+/*!
+       * (C) Ionic http://ionicframework.com - MIT License
+       */const i=()=>{const n=window;n.addEventListener("statusTap",()=>{(0,o.e)(()=>{const l=n.innerWidth,c=n.innerHeight,r=document.elementFromPoint(l/2,c/2);if(!r)return;const t=(0,_.f)(r);t&&new Promise(E=>(0,d.c)(t,E)).then(()=>{(0,o.w)((0,a.A)(function*(){t.style.setProperty("--overflow","hidden"),yield(0,_.s)(t,300),t.style.removeProperty("--overflow")}))})})})}}}]);
+//# sourceMappingURL=1791.03b3f8ecc1cd24cf.js.map
