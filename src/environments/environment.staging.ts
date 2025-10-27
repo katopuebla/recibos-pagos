487c566b39f3ca5adacaa558510c185e43ea6503
Kato <katopuebla@gmail.com>
2025-10-27 03:09:10 -0600

    change: Se agregó staging como ambiente

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,10 @@
+// src/environments/environment.staging.ts
+export const environment = {
+  production: false,                     // sigue sin ser producción
+  name: 'staging',
+  baseHref: '/recibos-pagos/staging',
+  apiUrl: 'https://katopuebla.github.io/recibos-pagos/staging',
+  apiTimeout: 30000,
+  defaultLanguage: 'es'
+};
+
