a96471805ad6480602818e04a1cac26febd1f94f
katopuebla <katopuebla@gmail.com>
2025-10-27 10:06:56 +0000

    Deploying to gh-pages from @ katopuebla/recibos-pagos@b7a77c86e2231c90f2175c9e366a3c83e6fc2756 🚀

---
+++ staging/4312.0ecd932eae05c44f.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4312],{4312:(l,s,n)=>{n.r(s),n.d(s,{ion_segment_content:()=>t});var e=n(1734);
+/*!
+       * (C) Ionic http://ionicframework.com - MIT License
+       */const t=class{constructor(o){(0,e.r)(this,o)}render(){return(0,e.h)(e.f,{key:"03684b2999ac64fe13e376fd7e7f279976e9d4f2"},(0,e.h)("slot",{key:"143031075bf33ca19e7cfd76fc8a67b83ccaf11c"}))}};t.style=":host{scroll-snap-align:center;scroll-snap-stop:always;-ms-flex-negative:0;flex-shrink:0;width:100%}"}}]);
+//# sourceMappingURL=4312.0ecd932eae05c44f.js.map
